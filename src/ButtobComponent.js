e1ed556a2450ddcf1a6b50a2ff3aeef45ee5a3b8
Geetha Madhuri <geethagorantla20@gmail.com>
2024-02-11 13:38:29 +0530

    @geethamadhuri Added package

---
+++ src/ButtobComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtobComponent = (props) => {
+    const {label}=props
+  return (
+    <div>
+      <button>{label}</button>
+    </div>
+  )
+}
+
+export default ButtobComponent
